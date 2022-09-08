db0617be5900245d272dc4f1048c800e803450c6
Phanith NY <ny.phanith.fe@gmail.com>
2022-09-08 19:45:14 +0700

    Rollback XCFramework

---
+++ Sources/CaptureProtectionView.xcframework/ios-arm64_x86_64-simulator/CaptureProtectionViewBinary.framework/Headers/CaptureProtectionViewBinary.h
@@ -0,0 +1,18 @@
+//
+//  CaptureProtectionViewBinary.h
+//  CaptureProtectionViewBinary
+//
+//  Created by Suykorng on 3/9/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaptureProtectionViewBinary.
+FOUNDATION_EXPORT double CaptureProtectionViewBinaryVersionNumber;
+
+//! Project version string for CaptureProtectionViewBinary.
+FOUNDATION_EXPORT const unsigned char CaptureProtectionViewBinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaptureProtectionViewBinary/PublicHeader.h>
+
+
